c428ccd59d1fe3c6dfacb21ff5fe650c0e19d1a1
Kasa74 <47076603+Kasa74@users.noreply.github.com>
2023-02-22 15:44:30 +0300

    Add files via upload

---
+++ ПР 2.1/circle.h
@@ -0,0 +1,19 @@
+class Triangle {
+private:
+    double a, b, c;
+
+public:
+    bool exst_tr();
+
+
+    void set(double a1, double b1, double c1);
+
+
+    void show();
+
+
+    double perimetr();
+
+
+    double square();
+};
